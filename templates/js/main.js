8ea8fe922bddd6185ea6a5bfecf827ea1f6793f1
Mohtad Ahmed Saeem <mohtadsaeem13@gmail.com>
2022-02-24 21:29:00 +0600

    code upload

---
+++ templates/js/main.js
@@ -0,0 +1,7 @@
+function fileSubmit() {
+        let photo = document.getElementById("image-file").files[0];
+        let formData = new FormData();
+
+        formData.append("photo", photo);
+        fetch("/upload/image", { method: "POST", body: formData });
+      }
