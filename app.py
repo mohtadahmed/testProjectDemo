ffba14fb9b8e05e736c98e17043d71a4189c95ee
Mohtad Ahmed Saeem <mohtadsaeem13@gmail.com>
2022-02-24 23:51:02 +0600

    POST Method worked

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
+from fileinput import filename
+from importlib.resources import path
+import os
 from flask import Flask, redirect, url_for, render_template, request
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    # if request.method == 'POST':
-    #     # Handle POST Request here
-    #     return render_template('index.html')
+    if request.method == 'POST':
+        # Handle POST Request here
+        if request.files:
+            f = request.files['file']
+            filename = secure_filename(f.filename)
+            path = 'upload/file'
+            filePath = os.path.join(path, filename)
+            f.save(filePath)
+            return render_template('home.html')
     return render_template('index.html')
 
 
--- templates/js/main.js
+++ templates/js/main.js
@@ -1,7 +1,19 @@
-function fileSubmit() {
-        let photo = document.getElementById("image-file").files[0];
-        let formData = new FormData();
+// function fileSubmit() {
+//         let photo = document.getElementById("image-file").files[0];
+//         let formData = new FormData();
 
-        formData.append("photo", photo);
-        fetch("/upload/image", { method: "POST", body: formData });
-      }
+//         formData.append("photo", photo);
+//         fetch("/upload/image", { method: "POST", body: formData });
+//       }
+
+
+
+// async function fileSubmit() {
+//     let formData = new FormData();           
+//     formData.append("file", fileupload.files[0]);
+//     await fetch('/upload/image', {
+//       method: "POST", 
+//       body: formData
+//     });    
+//     alert('The file has been uploaded successfully.');
+// }
