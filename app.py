8c8ba598ae6832bb9dc4ac7e4622d6a8cfb6e584
Mohtad Ahmed Saeem <mohtadsaeem13@gmail.com>
2022-02-28 23:44:45 +0600

    file and directory existence, pdf page counter

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from fileinput import filename
+from http.client import OK
 from importlib.resources import path
 import os
 from flask import Flask, redirect, url_for, render_template, request
 from werkzeug.utils import secure_filename
+import PyPDF2
+
 
 app = Flask(__name__)
 
@@ -13,11 +16,41 @@ def home():
         # Handle POST Request here
         if request.files:
             f = request.files['file']
+            name = str(request.form['person'])
             filename = secure_filename(f.filename)
-            path = 'upload/file'
+            path = 'upload/file/' + name
+            # x = os.mkdir(path)
+
+            # IF Else loop for checking the directory exists or not
+            if os.path.isdir(path) == True:
+                print(path)
+            else:
+                os.mkdir(path)
+
+            # If Else Loop Ends
+
+            print(path)
             filePath = os.path.join(path, filename)
-            f.save(filePath)
-            return render_template('home.html')
+
+            # IF Else loop for checking the same name file exists in the directory
+            if os.path.isfile(filePath) == True:
+                os.remove(filePath)
+                print('work on IF')
+                f.save(filePath)
+            else:
+                f.save(filePath)
+                print('Work on Else')
+            print(filePath)
+            # IF Else loop for checking the same name file exists in the directory
+
+            # Code for PDF Page Counter
+            sample_pdf = open(filePath, mode='rb')
+            pdfdoc = PyPDF2.PdfFileReader(sample_pdf)
+            print(pdfdoc.numPages)
+            pageNumber = pdfdoc.numPages
+            # Code for PDF Page Counter
+
+            return render_template('home.html', pageNumber=pageNumber)
     return render_template('index.html')
 
 
---
+++ demo.py
@@ -0,0 +1,10 @@
+# import PyPDF2
+# sample_pdf = open(r'C:\sample.pdf', mode='rb')
+# pdfdoc = PyPDF2.PdfFileReader(sample_pdf)
+# print(pdfdoc.numPages)
+
+
+import os
+
+isFile = os.path.isdir("upload/file/saeem")
+print(isFile)
